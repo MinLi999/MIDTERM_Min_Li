e99cc8f495398d04059a0d6d6b834623c4325ef0
Min Li <presley.us@gmail.com>
2024-10-29 16:19:35 -0400

    Initial Commit

---
+++ MIDTERM_Min_Li/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MIDTERM_Min_Li
+//
+//  Created by Elvis Li on 2024-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MIDTERM_Min_Li/MIDTERM_Min_LiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDTERM_Min_LiApp.swift
+//  MIDTERM_Min_Li
+//
+//  Created by Elvis Li on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct MIDTERM_Min_LiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
